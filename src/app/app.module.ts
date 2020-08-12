0da9e9d3d764ca1c1a24b88f038709e0899d29bb
Pardeshi <797609@cognizant.com>
2020-08-12 22:27:22 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
+
+import { BookService } from './shared/book.service';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddBookComponent,
+    EditBookComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [BookService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBnxkYJBA6cDXG7FcP-ZXIr0TF1SAd7y7A",
+    authDomain: "library-management-17f95.firebaseapp.com",
+    databaseURL: "https://library-management-17f95.firebaseio.com",
+    projectId: "library-management-17f95",
+    storageBucket: "library-management-17f95.appspot.com",
+    messagingSenderId: "25679102009",
+    appId: "1:25679102009:web:8174a930bf97c256cc8de8",
+    measurementId: "G-KHG1X0ZYFN"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
